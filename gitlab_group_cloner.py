bd97533dfa96293fd67412f93acef3620299aa5f
Blake Derbin <blake.derbin@hotmail.com>
2021-09-23 13:38:48 +1000

    error handling fixes

--- gitlab_group_cloner.py
+++ gitlab_group_cloner.py
@@ -19,7 +19,6 @@ user_args = parser.parse_args()
 auth_token = user_args.token
 group_id = user_args.group
 api_version = (user_args.apiversion, 'v4')[user_args.apiversion is None]
-
 clone_base_url = f'https://oauth2:{user_args.token}@gitlab.com/'
 api_url = f'https://gitlab.com/api/{api_version}' 
 api_group_projects = f'{api_url}/groups/{group_id}/projects?private_token={auth_token}&include_subgroups=true'
@@ -45,30 +44,35 @@ def handle_remove_readonly(func, path, exc):
 
 
 def fetch_group_projects():
-    request = requests.get(api_group_projects)
-    data = json.loads(request.text)
     try:
+        request = requests.get(api_group_projects)
+        data = json.loads(request.text)
+
         for index in range(len(data)):
             for key in data[index]:
                 if key == 'http_url_to_repo':
                     gitlab_group_project_link.append(data[index]['http_url_to_repo'])
                 if key == 'path_with_namespace':
                     gitlab_group_path_namespace.append(data[index]['path_with_namespace'].split('/',1))
+
         log_file.write(f"Successfully fetched projects for group ID: {group_id}\n")
-    except:
-        log_file.write(f"Unable to fetch group ID: {group_id} projects, exiting script...\n")
+
+    except OSError as e:
+        log_file.write(f"Unable to fetch group ID: {group_id} projects, exiting script...\nError: {e}\n")
         log_file.close()
         sys.exit(1)
 
 
 def make_backup_directory():
-    path_exists = os.path.exists(directory_path)
     try:
+        path_exists = os.path.exists(directory_path)
+
         if not path_exists:
             os.makedirs(directory_path)
             log_file.write(f"directory created: {directory_path}\n")
-    except:
-        log_file.write(f"Unable to create {directory_path}, exiting script...\n")
+
+    except OSError as e:
+        log_file.write(f"Unable to create {directory_path}, exiting script...\nError: {e}\n")
         log_file.close()
         sys.exit(1)
 
@@ -103,13 +107,14 @@ def backup_group_projects():
                 os.chdir(directory_path)
                 git.Git().clone(clone_base_url + p.split("https://gitlab.com/")[1],
                                 os.path.join(directory_path,gitlab_group_path_namespace[count][1]))
-                log_file.write(f"cloned repository: {repo_name}\n")
+                log_file.write(f"cloned repository: {repository_name}\n")
 
             count += 1
 
         log_file.write(f"\nAll repositories for group ID: {group_id} have been backed up\n\n")
-    except:
-        log_file.write(f"Unable to backup projects for group ID: {group_id}, exiting script...\n")
+
+    except OSError as e:
+        log_file.write(f"Unable to backup projects for group ID: {group_id}, exiting script...\nError: {e}\n")
         log_file.close()
         sys.exit(1)
 
@@ -123,28 +128,26 @@ def backup_group_projects_to_tar():
         tar_file = os.path.join(tarfile_path, tar_filename)
 
         tar_file_exists = os.path.exists(os.path.abspath(tar_file))
-        directory_path_exists = os.path.exists(os.path.abspath(directory_path))
 
-        if directory_path_exists:
-            if tar_file_exists:
-                log_file.write(f"{tar_filename} exists, no new tarfile will be generated\n")
-            else:
-                tar_backup = tarfile.open(tar_filename, 'w|gz')
-                tar_backup.add(directory_path, recursive=True, arcname=backup_path)
-                tar_backup.close()
-                log_file.write(f"Created tar backup file of repositories at: {tar_file}\n")
+        if tar_file_exists:
+            log_file.write(f"{tar_filename} exists, no new tarfile will be generated\n")
         else:
-            log_file.write("backup directory doesn't exist, exiting script...\n")
-            log_file.close()
-            sys.exit(1)
-    except:
-        log_file.write(f"Error has occured adding backups to tar file, exiting...")
+            tar_backup = tarfile.open(tar_filename, 'w|gz')
+            tar_backup.add(directory_path, recursive=True, arcname=backup_path)
+            tar_backup.close()
+            log_file.write(f"Created tar backup file of repositories at: {tar_file}\n")
+
+    except OSError as e:
+        log_file.write(f"Error has occured adding backups to tar file, exiting...\nError: {e}\n")
         log_file.close()
         sys.exit(1)
 
+try:
+    fetch_group_projects()
+    make_backup_directory()
+    backup_group_projects()
+    backup_group_projects_to_tar()
+    log_file.close()
 
-fetch_group_projects()
-make_backup_directory()
-backup_group_projects()
-backup_group_projects_to_tar()
-log_file.close()
+except OSError as e:
+    log_file.write(f"OS error has occured: \n\n{e}")
